becb36762f1ae05cef639b3bf34eb6e4b64d8abd
ireshdilsh <ireshdilshan46@gmail.com>
2024-02-09 14:14:28 +0530

    adding post api

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,45 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  UserArray :any[]=[];
+
+  userid :number=0;
+  usersname :string="";
+  useremail :string="";
+  userusername:string="";
+  userpassword:string="";
+
+  constructor(private http:HttpClient){
+    this.getAllUsers();
+  }
+
+  getAllUsers(){
+
+  }
+
+  reset(){
+    this.userid=0;
+         this.usersname='';
+         this.useremail='';
+         this.userusername='';
+         this.userpassword='';
+  }
+
    onSubmit(){
+     let data={
+        "id":this.userid,
+        "name":this.usersname,
+      //  "email":this.useremail,
+        "username":this.userusername,
+        "passsword":this.userpassword
+     };
+
+        this.http.post("http://localhost:8080/add/user",data,{responseType:'text'}).subscribe((resultData:any)=>{
+         console.log(resultData);
+         alert("User Save Success");
+         this.reset();
+      });
+     
 
   }
 
